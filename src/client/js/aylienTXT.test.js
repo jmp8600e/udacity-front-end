3edf08c1f65da90d73b4e754615ac4db6b70a947
Jatin Patel <jmp8600e@gmail.com>
2020-01-09 11:39:56 -0500

    - fixing test js file for Jest to work

--- src/client/js/aylienTXT.test.js
+++ src/client/js/aylienTXT.test.js
@@ -1,4 +1,4 @@
-const testtxtapi = require('./testapi');
+const testtxtapi = require('./aylienTXT');
 
 test('Testing text sentiment api', () => {
   expect(testtxtapi(encodeURIComponent("i am feeling good!"))).toBe(4);
